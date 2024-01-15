b54edf92927bc58b41a9b117fd0cbaea115ff194
miyake-yuki <cle.neige@gmail.com>
2024-01-15 16:04:50 +0900

    improve

--- impls/rust/src/read.rs
+++ impls/rust/src/read.rs
@@ -1,3 +1,5 @@
+use std::iter::Peekable;
+
 use self::special_string::*;
 
 macro_rules! regex {
@@ -63,32 +65,31 @@ enum Token {
     STR(MaybeUncloedString),
     SYM(String),
     KEYWORD(String),
+    SPECIAL(Specials),
+    BRACKET(Kind, State),
+    MACRO(MacroChar),
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Kind {
+    Paren,  // ()
+    Square, // []
+    Curly,  // {}
+}
+
+enum State {
+    Open,
+    Close,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Specials {
     DEF,
     DO,
     IF,
     LET,
     FN,
-    ROUND(Bracket),  // ()
-    SQUARE(Bracket), // []
-    CURLY(Bracket),  // {}
-    MACRO(MacroChar),
 }
-
-enum Bracket {
-    Open,
-    Close,
-}
-
-// enum Kind {
-//     Paren,  // ()
-//     Square, // []
-//     Curly,  // {}
-// }
-
-// enum State {
-//     Open,
-//     Close,
-// }
 
 enum MacroChar {
     Quote,           // '
@@ -103,28 +104,28 @@ enum MacroChar {
 fn tokenize<I>(input: &str) -> impl Iterator<Item = Token> + '_ {
     regex!(r#"[\s,]*(;.*|~@|[\[\]{}()'`~^@]|"(?:\\.|[^\\"])*"?|[^\s\[\]{}()'"`,;]*)"#)
         .captures_iter(input)
-        // cap[0]はマッチした文字列全体, cap[1]はグループ化した文字列=空白以外の部分
+        // cap[0]:マッチした文字列全体, cap[1]:グループ化した文字列=空白以外の部分
         .map(|cap| unsafe { cap.get(1).unwrap_unchecked() }.as_str())
         .filter(|token| !token.starts_with(';')) // コメント行を除外
-        .map(|token| read_token(token))
+        .map(|t| token(t))
 }
 
-fn read_token(token: &str) -> Token {
-    match token {
+fn token(s: &str) -> Token {
+    match s {
         "nil" => Token::NIL,
         "true" => Token::BOOL(true),
         "false" => Token::BOOL(false),
-        "def" => Token::DEF,
-        "do" => Token::DO,
-        "if" => Token::IF,
-        "let" => Token::LET,
-        "fn" => Token::FN,
-        "(" => Token::ROUND(Bracket::Open),
-        ")" => Token::ROUND(Bracket::Close),
-        "[" => Token::SQUARE(Bracket::Open),
-        "]" => Token::SQUARE(Bracket::Close),
-        "{" => Token::CURLY(Bracket::Open),
-        "}" => Token::CURLY(Bracket::Close),
+        "def" => Token::SPECIAL(Specials::DEF),
+        "do" => Token::SPECIAL(Specials::DO),
+        "if" => Token::SPECIAL(Specials::IF),
+        "let" => Token::SPECIAL(Specials::LET),
+        "fn" => Token::SPECIAL(Specials::FN),
+        "(" => Token::BRACKET(Kind::Paren, State::Open),
+        ")" => Token::BRACKET(Kind::Paren, State::Close),
+        "[" => Token::BRACKET(Kind::Square, State::Open),
+        "]" => Token::BRACKET(Kind::Square, State::Close),
+        "{" => Token::BRACKET(Kind::Curly, State::Open),
+        "}" => Token::BRACKET(Kind::Curly, State::Close),
         "'" => Token::MACRO(MacroChar::Quote),
         "`" => Token::MACRO(MacroChar::Quasiquote),
         "~" => Token::MACRO(MacroChar::Unquote),
@@ -133,14 +134,14 @@ fn read_token(token: &str) -> Token {
         "#" => Token::MACRO(MacroChar::Dispatch),
         "^" => Token::MACRO(MacroChar::Meta),
         _ => {
-            if let Ok(n) = token.parse::<i64>() {
+            if let Ok(n) = s.parse::<i64>() {
                 Token::NUM(n)
-            } else if token.starts_with('"') {
-                Token::STR(MaybeUncloedString::new(token.to_string()))
-            } else if token.starts_with(':') {
-                Token::KEYWORD(token[1..].to_string())
+            } else if s.starts_with('"') {
+                Token::STR(MaybeUncloedString::new(s.to_string()))
+            } else if s.starts_with(':') {
+                Token::KEYWORD(s[1..].to_string())
             } else {
-                Token::SYM(token.to_string())
+                Token::SYM(s.to_string())
             }
         }
     }
@@ -172,10 +173,11 @@ enum SpecialForms {
     Fn(Vec<Symbol>, Option<Symbol>, Ast),
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 enum ParseError {
     UnexpectedEOF,
     Unclosed(Enclosure),
+    Def(String),
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -186,11 +188,21 @@ enum Enclosure {
     Quote,         // ""
 }
 
-fn read_ast<I>(next: Token, rest: &mut I) -> Result<Ast, ParseError>
+impl From<Kind> for Enclosure {
+    fn from(kind: Kind) -> Self {
+        match kind {
+            Kind::Paren => Enclosure::Paren,
+            Kind::Square => Enclosure::SquareBracket,
+            Kind::Curly => Enclosure::CurlyBracket,
+        }
+    }
+}
+
+fn read_form<I>(tokens: &mut Peekable<I>) -> Result<Ast, ParseError>
 where
     I: Iterator<Item = Token>,
 {
-    match next {
+    match tokens.next().ok_or(ParseError::UnexpectedEOF)? {
         Token::NIL => Ok(Ast::Nil),
         Token::BOOL(b) => Ok(Ast::Bool(b)),
         Token::NUM(n) => Ok(Ast::Num(n)),
@@ -200,46 +212,160 @@ where
         )),
         Token::SYM(s) => Ok(Ast::Sym(Symbol(s))),
         Token::KEYWORD(s) => Ok(Ast::Keyword(s)),
-        // Token::DEF => read_def(rest),
-        // Token::DO => read_do(rest),
-        // Token::IF => read_if(rest),
-        // Token::LET => read_let(rest),
-        // Token::FN => read_fn(rest),
-        Token::ROUND(Bracket::Open) => match rest.next() {
-            Some(Token::DEF) => Ok(Ast::SpecialForm(Box::new(read_def(rest)?))),
-            _ => read_list(rest),
-        },
-        // Token::SQUARE(Bracket::Open) => read_list(rest).map(Ast::Vector),
+        Token::BRACKET(Kind::Paren, State::Open) => {
+            if let Some(&Token::SPECIAL(s)) = tokens.peek() {
+                Ok(Ast::SpecialForm(Box::new(read_special_form(s, tokens)?)))
+            } else {
+                read_seq(tokens, Kind::Paren).map(Ast::List)
+            }
+        }
+        Token::BRACKET(Kind::Square, State::Open) => {
+            read_seq(tokens, Kind::Square).map(Ast::Vector)
+        }
         // Token::CURLY(Bracket::Open) => read_map(rest).map(Ast::Map),
         // Token::MACRO(c) => read_macro(c, rest),
-        _ => unreachable!(),
+        _ => todo!(),
     }
 }
 
-fn read_def<I>(tokens: &mut I) -> Result<SpecialForms, ParseError>
+fn read_seq<I>(tokens: &mut Peekable<I>, kind: Kind) -> Result<Vec<Ast>, ParseError>
+where
+    I: Iterator<Item = Token>,
+{
+    let mut seq = Vec::new();
+    loop {
+        match tokens.peek() {
+            Some(&Token::BRACKET(k, State::Close)) if k == kind => {
+                tokens.next();
+                return Ok(seq);
+            }
+            Some(_) => seq.push(read_form(tokens)?),
+            None => return Err(ParseError::Unclosed(kind.into())),
+        }
+    }
+}
+
+fn read_special_form<I>(
+    atom: Specials,
+    tokens: &mut Peekable<I>,
+) -> Result<SpecialForms, ParseError>
+where
+    I: Iterator<Item = Token>,
+{
+    tokens.next(); // skip atom
+    let f = match atom {
+        Specials::DEF => read_def(tokens),
+        // read_seqで")"を処理するためreturn
+        Specials::DO => return read_seq(tokens, Kind::Paren).map(SpecialForms::Do),
+        Specials::IF => read_if(tokens),
+        Specials::LET => read_let(tokens),
+        Specials::FN => read_fn(tokens),
+    }?;
+
+    if let Some(Token::BRACKET(Kind::Paren, State::Close)) = tokens.next() {
+        Ok(f)
+    } else {
+        Err(ParseError::Unclosed(Enclosure::Paren))
+    }
+}
+
+fn read_def<I>(tokens: &mut Peekable<I>) -> Result<SpecialForms, ParseError>
 where
     I: Iterator<Item = Token>,
 {
     let sym = match tokens.next() {
         Some(Token::SYM(s)) => s,
-        _ => return Err(ParseError::UnexpectedEOF),
+        Some(_) => {
+            return Err(ParseError::Def(
+                "First argument to def must be a Symbol".to_string(),
+            ))
+        }
+        None => return Err(ParseError::Def("too few arguments to def".to_string())),
     };
-    Ok(SpecialForms::Def(
-        read_ast(tokens.next().ok_or(ParseError::UnexpectedEOF)?, tokens)?,
-        read_ast(tokens.next().ok_or(ParseError::UnexpectedEOF)?, tokens)?,
-    ))
+
+    // TODO: 二引数目のエラー処理
+    Ok(SpecialForms::Def(Symbol(sym), read_form(tokens)?))
 }
 
-fn read_list<I>(tokens: &mut I) -> Result<Ast, ParseError>
+// TODO
+fn read_if<I>(tokens: &mut Peekable<I>) -> Result<SpecialForms, ParseError>
 where
     I: Iterator<Item = Token>,
 {
-    let mut list = Vec::new();
+    let cond = read_form(tokens)?;
+    let then = read_form(tokens)?;
+    let els = read_form(tokens)?;
+    Ok(SpecialForms::If(cond, then, els))
+}
+
+// TODO
+fn read_let<I>(tokens: &mut Peekable<I>) -> Result<SpecialForms, ParseError>
+where
+    I: Iterator<Item = Token>,
+{
+    let mut bindings = Vec::new();
     loop {
-        match tokens.next() {
-            Some(Token::ROUND(Bracket::Close)) => return Ok(Ast::List(list)),
-            Some(token) => list.push(read_ast(token, tokens)?),
+        match tokens.peek() {
+            Some(Token::BRACKET(Kind::Paren, State::Close)) => {
+                tokens.next();
+                break;
+            }
+            Some(_) => {
+                let sym = match tokens.next() {
+                    Some(Token::SYM(s)) => s,
+                    Some(_) => {
+                        return Err(ParseError::Def(
+                            "First argument to def must be a Symbol".to_string(),
+                        ))
+                    }
+                    None => return Err(ParseError::Def("too few arguments to def".to_string())),
+                };
+                let ast = read_form(tokens)?;
+                bindings.push((Symbol(sym), ast));
+            }
             None => return Err(ParseError::Unclosed(Enclosure::Paren)),
         }
     }
+    let body = read_form(tokens)?;
+    Ok(SpecialForms::Let(bindings, body))
+}
+
+// TODO
+fn read_fn<I>(tokens: &mut Peekable<I>) -> Result<SpecialForms, ParseError>
+where
+    I: Iterator<Item = Token>,
+{
+    let mut params = Vec::new();
+    loop {
+        match tokens.peek() {
+            Some(Token::BRACKET(_, State::Close)) => {
+                tokens.next();
+                break;
+            }
+            Some(_) => {
+                let sym = match tokens.next() {
+                    Some(Token::SYM(s)) => s,
+                    Some(_) => {
+                        return Err(ParseError::Def(
+                            "First argument to def must be a Symbol".to_string(),
+                        ))
+                    }
+                    None => return Err(ParseError::Def("too few arguments to def".to_string())),
+                };
+                params.push(Symbol(sym));
+            }
+            None => return Err(ParseError::Unclosed(Enclosure::Paren)),
+        }
+    }
+    let rest = match tokens.next() {
+        Some(Token::SYM(s)) => Some(Symbol(s)),
+        Some(_) => {
+            return Err(ParseError::Def(
+                "First argument to def must be a Symbol".to_string(),
+            ))
+        }
+        None => None,
+    };
+    let body = read_form(tokens)?;
+    Ok(SpecialForms::Fn(params, rest, body))
 }
